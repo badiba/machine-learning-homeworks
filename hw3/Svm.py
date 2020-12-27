365645daf70a29d15c36a6e69c0ba340313b66ab
Bahadir Coskun <53962512+badiba@users.noreply.github.com>
2020-12-27 20:01:50 +0300

    add hyperparameter optimization for hw3 part 3

--- hw3/Svm.py
+++ hw3/Svm.py
@@ -1,11 +1,14 @@
 import numpy as np
 import os
+import sys
 from draw import draw_svm
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
 
 
 class SVM:
-    def __init__(self):
+    def __init__(self, extraLoad=None):
         dirname = os.path.dirname(os.path.abspath(__file__))
         self._trainDataSep = np.load(os.path.join(
             dirname, "hw3_data", "linsep", "train_data.npy"))
@@ -16,7 +19,31 @@ class SVM:
         self._trainLabelsNonSep = np.load(os.path.join(
             dirname, "hw3_data", "nonlinsep", "train_labels.npy"))
 
+        if (extraLoad != None):
+            self._trainData = np.load(os.path.join(
+                dirname, "hw3_data", extraLoad, "train_data.npy"))
+            self._trainLabels = np.load(os.path.join(
+                dirname, "hw3_data", extraLoad, "train_labels.npy"))
+            self._testData = np.load(os.path.join(
+                dirname, "hw3_data", extraLoad, "test_data.npy"))
+            self._testLabels = np.load(os.path.join(
+                dirname, "hw3_data", extraLoad, "test_labels.npy"))
+
+            normalizationFactor = (2.0 / 255.0)
+
+            self._trainData = self._trainData.astype(np.float)
+            for example in self._trainData:
+                for i in range(len(example)):
+                    example[i] = example[i] * normalizationFactor - 1
+
+            self._testData = self._testData.astype(np.float)
+            for example in self._testData:
+                for i in range(len(example)):
+                    example[i] = example[i] * normalizationFactor - 1
+
         self._kernels = ["linear", "rbf", "poly", "sigmoid"]
+        self._cValues = [0.01, 0.1, 1, 10, 100]
+        self._gamma = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
 
     def TrainLinearlySeparable(self):
         dirname = os.path.dirname(os.path.abspath(__file__))
@@ -41,10 +68,43 @@ class SVM:
             draw_svm(svc, self._trainDataSep,
                      self._trainLabelsSep, -3, 3, -3, 3, savePath)
 
+    def Train(self):
+        parameters = {'kernel': self._kernels, 'C': self._cValues, 'gamma': self._gamma}
+        svc = SVC()
+        clf = GridSearchCV(svc, parameters)
+        clf.fit(self._trainData, self._trainLabels)
+        
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        print(pd.concat([pd.DataFrame(clf.cv_results_["params"]), pd.DataFrame(
+            clf.cv_results_["mean_test_score"], columns=["Accuracy"])], axis=1))
+
 
 def main():
-    supportVectorMachine = SVM()
-    supportVectorMachine.TrainLinearlySeparable()
+    if (len(sys.argv) == 2):
+        argument = sys.argv[1]
+
+        if (argument == "3-1"):
+            supportVectorMachine = SVM()
+            supportVectorMachine.TrainLinearlySeparable()
+            print("Figures are saved to the execution path.")
+
+        elif (argument == "3-2"):
+            supportVectorMachine = SVM()
+            supportVectorMachine.TrainLinearlyNonSeparable()
+            print("Figures are saved to the execution path.")
+
+        elif (argument == "catdog" or argument == "catdogimba"):
+            supportVectorMachine = SVM(argument)
+            supportVectorMachine.Train()
+            
+        else:
+            print("Wrong argument. See README file. Terminating...")
+            exit()
+    else:
+        print("Wrong argument. See README file. Terminating...")
+        exit()
+
 
 
 if __name__ == '__main__':
