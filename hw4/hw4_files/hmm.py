4264cd54b018aeffd3a6913c07081fa4e9697200
Bahadir Coskun <53962512+badiba@users.noreply.github.com>
2021-01-29 19:29:11 +0300

    add hmm viterbi algorithm for hw4

--- hw4/hw4_files/hmm.py
+++ hw4/hw4_files/hmm.py
@@ -55,3 +55,50 @@ def viterbi(A, B, pi, O):
     :return: The most likely state sequence with shape (T,) and the calculated deltas in the Trellis diagram with shape
              (N, T). They should be numpy arrays.
     """
+
+    N = len(pi)
+    T = len(O)
+
+    delta = np.zeros((N, T))
+    parents = np.zeros((N, T), dtype=int)
+
+    for state in range(N):
+        delta[state][0] = pi[state] * B[state][O[0]]
+
+    i = 1
+    while (i < T):
+        for j in range(N):
+            maxValue = -1
+            maxIndex = -1
+
+            for k in range(N):
+                nodeValue = delta[k][i - 1] * A[k][j] * B[j][O[i]]
+                if (nodeValue > maxValue):
+                    maxValue = nodeValue
+                    maxIndex = k
+
+            delta[j][i] = maxValue
+            parents[j][i] = maxIndex
+
+        i += 1
+
+    viterbiResult = np.zeros((T,), dtype=int)
+
+    maxValue = -1
+    maxIndex = -1
+    for i in range(N):
+        if (delta[i][T - 1] > maxValue):
+            maxValue = delta[i][T - 1]
+            maxIndex = i
+
+    i = T - 2
+    tracker = maxIndex
+    while i >= 0:
+        viterbiResult[i] = parents[tracker][i + 1]
+        tracker = viterbiResult[i]
+
+        i -= 1
+
+    viterbiResult[T - 1] = maxIndex
+
+    return viterbiResult, delta
