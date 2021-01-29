9547334e297b4ecc5fc067e44462cd6755e7ba58
Bahadir Coskun <53962512+badiba@users.noreply.github.com>
2021-01-29 18:57:38 +0300

    add hmm forward algorithm in hw4

--- hw4/hw4_files/hmm.py
+++ hw4/hw4_files/hmm.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def forward(A, B, pi, O):
     """
     Calculates the probability of an observation sequence O given the model(A, B, pi).
@@ -11,6 +14,32 @@ def forward(A, B, pi, O):
     :return: The probability of the observation sequence and the calculated alphas in the Trellis diagram with shape
              (N, T) which should be a numpy array.
     """
+
+    N = len(pi)
+    T = len(O)
+
+    alpha = np.zeros((N, T))
+
+    for state in range(N):
+        alpha[state][0] = pi[state] * B[state][O[0]]
+
+    i = 1
+    while (i < T):
+        for j in range(N):
+            nodeValue = 0
+
+            for k in range(N):
+                nodeValue += alpha[k][i - 1] * A[k][j] * B[j][O[i]]
+
+            alpha[j][i] = nodeValue
+
+        i += 1
+
+    forwardResult = 0
+    for i in range(N):
+        forwardResult += alpha[i][T - 1]
+
+    return forwardResult, alpha
 
 
 def viterbi(A, B, pi, O):
